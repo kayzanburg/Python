4f6ebd210e5a1d59840c299b06099a8cb8e27fcd
Bilal Arslan <bilal-arsln@hotmail.com>
2022-05-22 06:28:45 +0300

    Python

---
+++ Yildiz_Ile_H_Harfi.py
@@ -0,0 +1,34 @@
+for i in range(12):
+    for j in range(11):
+        print(" ",end="")
+        
+    if i == 5:
+        if i == 5:
+        
+            for y in range(16):
+                print("x",end="")
+            print()
+            
+            
+    elif i == 6:
+        if i == 6:
+        
+            for y in range(16):
+                print("x",end="")
+            print()
+        
+    else:
+    
+
+        for j in range(3):
+            print("x",end="")
+        
+        for m in range(10):
+            print(" ",end="")
+    
+    
+        for b in range(3):
+            print("x",end="")
+
+
+        print()
